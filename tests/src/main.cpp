f5ab14185ea83529c1916fe4a038f88216758909
toni-schmitt <tonischmitt@online.de>
2022-10-23 18:14:38 +0200

    ADD (tests): run_generic_tests function
    Signed-off-by: toni-schmitt <tonischmitt@online.de>

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -2,8 +2,15 @@
 #include "tests.hpp"
 #include "generic/run_test.hpp"
 
+template < class Container >
+static void run_generic_tests()
+{
+	generic::run_test<generic::construction::construction_test<Container> >("construction");
+	generic::run_test<generic::capacity::capacity_test<Container> >("capacity");
+	generic::run_test<generic::iterator::iterator_test<Container> >("iterator");
+}
+
 int main()
 {
-	generic::run_test<generic::construction::construction_test<NAMESPACE::vector<TYPE> > >("construction_test");
-
+	run_generic_tests<NAMESPACE::vector<TYPE> >();
 }
