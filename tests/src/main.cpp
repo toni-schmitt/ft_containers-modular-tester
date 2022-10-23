824f07ee7800d7ef897fa889885a7bdfbb829997
toni-schmitt <tonischmitt@online.de>
2022-10-23 19:56:32 +0200

    ADD (tests): Now does not compile separate ft and std binaries, now calls ft and std tests in main
    Signed-off-by: toni-schmitt <tonischmitt@online.de>

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -2,6 +2,30 @@
 #include "tests.hpp"
 #include "generic/run_test.hpp"
 
+#include <fstream>
+#include <iterator>
+#include <string>
+#include <algorithm>
+
+bool compare_files(const std::string &file_1, const std::string &file_2)
+{
+	std::ifstream file_stream_1(file_1, std::ifstream::binary | std::ifstream::ate);
+	std::ifstream file_stream_2(file_2, std::ifstream::binary | std::ifstream::ate);
+
+	if (file_stream_1.fail() || file_stream_2.fail())
+		return false; //file problem
+
+	if (file_stream_1.tellg() != file_stream_2.tellg())
+		return false; //size mismatch
+
+	//seek back to beginning and use std::equal to compare contents
+	file_stream_1.seekg(0, std::ifstream::beg);
+	file_stream_2.seekg(0, std::ifstream::beg);
+	return std::equal(std::istreambuf_iterator<char>(file_stream_1.rdbuf()),
+					  std::istreambuf_iterator<char>(),
+					  std::istreambuf_iterator<char>(file_stream_2.rdbuf()));
+}
+
 template < class Container >
 static void run_generic_tests()
 {
@@ -10,9 +34,76 @@ static void run_generic_tests()
 	generic::run_test<generic::iterator::iterator_test<Container> >("iterator");
 }
 
+template < class ContainerSTD, class ContainerFT >
+static bool run_generic_construction_test()
+{
+	static const std::string test_case_name = "construction";
+
+	std::string std_log;
+	std::string ft_log;
+
+	generic::file_name_postfix = "std.log";
+	generic::run_test<generic::construction::construction_test<ContainerSTD> >(test_case_name);
+	std_log = generic::test_file_name;
+	generic::file_name_postfix = "ft.log";
+	generic::run_test<generic::construction::construction_test<ContainerFT> >(test_case_name);
+	ft_log = generic::test_file_name;
+	return compare_files(std_log, ft_log);
+}
+
+template < class ContainerSTD, class ContainerFT >
+static bool run_generic_capacity_test()
+{
+	static const std::string test_case_name = "capacity";
+
+	std::string std_log;
+	std::string ft_log;
+
+	generic::file_name_postfix = "std.log";
+	generic::run_test<generic::capacity::capacity_test<ContainerSTD> >(test_case_name);
+	std_log = generic::test_file_name;
+	generic::file_name_postfix = "ft.log";
+	generic::run_test<generic::capacity::capacity_test<ContainerFT> >(test_case_name);
+	ft_log = generic::test_file_name;
+	return compare_files(std_log, ft_log);
+}
+
+template < class ContainerSTD, class ContainerFT >
+static bool run_generic_iterator_test()
+{
+	static const std::string test_case_name = "iterator";
+
+	std::string std_log;
+	std::string ft_log;
+
+	generic::file_name_postfix = "std.log";
+	generic::run_test<generic::iterator::iterator_test<ContainerSTD> >(test_case_name);
+	std_log = generic::test_file_name;
+	generic::file_name_postfix = "ft.log";
+	generic::run_test<generic::iterator::iterator_test<ContainerFT> >(test_case_name);
+	ft_log = generic::test_file_name;
+	return compare_files(std_log, ft_log);
+}
+
+
+static void run_generic_tests()
+{
+	typedef ft_namespace::namespace_chooser ft_chooser;
+	typedef std_namespace::namespace_chooser std_chooser;
+
+	std::cout << "Running Generic Construction Test:" << std::endl;
+	std::cout << "Success: " << std::boolalpha
+			  << run_generic_construction_test<std_chooser::vector, ft_chooser::vector>()
+			  << std::endl;
+	std::cout << "Running Generic Capacity Test:" << std::endl;
+	std::cout << "Success: " << std::boolalpha << run_generic_capacity_test<std_chooser::vector, ft_chooser::vector>()
+			  << std::endl;
+	std::cout << "Running Generic Iterator Test:" << std::endl;
+	std::cout << "Success: " << std::boolalpha << run_generic_iterator_test<std_chooser::vector, ft_chooser::vector>()
+			  << std::endl;
+}
+
 int main()
 {
-	run_generic_tests<NAMESPACE::vector<TYPE> >();
-	run_generic_tests<NAMESPACE::map<TYPE, TYPE> >();
-	run_generic_tests<NAMESPACE::set<TYPE> >();
+	run_generic_tests();
 }
--- tests/src/tests.hpp
+++ tests/src/tests.hpp
@@ -1,21 +1,5 @@
 #pragma once
 
-
-// Defines ft/std Namespace (and file postfix)
-#if defined(STD) && STD == 1
-
-#define FILE_NAME_POSTFIX "_std.log"
-#define NAMESPACE std
-
-#elif defined(FT) && FT == 1
-
-#define NAMESPACE ft
-#define FILE_NAME_POSTFIX "_ft.log"
-
-#else
-#error "No Namespace Defined"
-#endif
-
 #include <fstream>
 #include <ostream>
 
@@ -25,7 +9,6 @@
 
 
 // Includes ft/std Containers
-#if defined(STD) && STD == 1
 
 #include <vector>
 #include <map>
@@ -34,29 +17,61 @@
 
 #undef STD
 
-#elif defined(FT) && FT == 1
-
 #include "vector.hpp"
 #include "map.hpp"
 #include "set.hpp"
 #include "stack.hpp"
 
-#undef FT
-
-#else
-#error "No Namespace Defined"
-#endif
 
 template < class T1, class T2 >
-std::ostream &operator<<(std::ostream &os, NAMESPACE::pair<T1, T2> pair)
+std::ostream &operator<<(std::ostream &os, ft::pair<T1, T2> pair)
 {
 	os << pair.first << ':' << pair.second;
 	return os;
 }
 
 template < class T1, class T2 >
-std::ofstream &operator<<(std::ofstream &ofs, NAMESPACE::pair<T1, T2> pair)
+std::ofstream &operator<<(std::ofstream &ofs, ft::pair<T1, T2> pair)
 {
 	ofs << pair.first << ':' << pair.second;
 	return ofs;
+}
+
+template < class T1, class T2 >
+std::ostream &operator<<(std::ostream &os, std::pair<T1, T2> pair)
+{
+	os << pair.first << ':' << pair.second;
+	return os;
+}
+
+template < class T1, class T2 >
+std::ofstream &operator<<(std::ofstream &ofs, std::pair<T1, T2> pair)
+{
+	ofs << pair.first << ':' << pair.second;
+	return ofs;
+}
+
+
+namespace std_namespace
+{
+	struct namespace_chooser
+	{
+		typedef std::vector<TYPE> vector;
+		typedef std::map<TYPE, TYPE> map;
+		typedef std::set<TYPE> set;
+		typedef std::stack<TYPE> stack;
+		typedef std::pair<TYPE, TYPE> pair;
+	};
+}
+
+namespace ft_namespace
+{
+	struct namespace_chooser
+	{
+		typedef ft::vector<TYPE> vector;
+		typedef ft::map<TYPE, TYPE> map;
+		typedef ft::set<TYPE> set;
+		typedef ft::stack<TYPE> stack;
+		typedef ft::pair<TYPE, TYPE> pair;
+	};
 }
