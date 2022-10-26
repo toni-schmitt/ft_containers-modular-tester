3fe4537fe3dfe1356247c4fd799ade9c21bdde09
toni-schmitt <tonischmitt@online.de>
2022-10-26 16:47:37 +0200

    ADD (tests): basic set-test structure
    Signed-off-by: toni-schmitt <tonischmitt@online.de>

---
+++ tests/src/tests/set/operations_test.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "../i_base_test.hpp"
+#include "../../utility/test_cases.hpp"
+
+namespace set
+{
+	template < class Container >
+	struct operations_test : public i_base_test<Container>
+	{
+		typedef typename i_base_test<Container>::container_type container_type;
+		typedef typename i_base_test<Container>::value_type value_type;
+
+		void test(out_file_stream *ofs = NULL)
+		{
+			if (ofs != NULL)
+			{
+				this->update_ofs(ofs);
+			}
+
+			this->print_test_start();
+
+			this->print_test_end();
+		}
+
+	private:
+	public:
+		explicit operations_test(
+				bool run_test_in_constructor = true
+		) : i_base_test<Container>("SetOperations")
+		{
+
+			if (!run_test_in_constructor)
+				return;
+
+			try
+			{
+				this->test();
+			}
+			catch (...)
+			{
+				throw;
+			}
+		}
+	};
+}
